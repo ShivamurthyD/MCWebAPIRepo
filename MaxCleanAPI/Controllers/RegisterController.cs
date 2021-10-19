5e39a2136428c0d9bc1f7233d87e0b9566a79cb5
ShivamurthyD <shivuvba1990@gmail.com>
2021-10-19 17:39:29 +0530

    Register Control Created

---
+++ MaxCleanAPI/Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MaxCleanAPI.DTO;
+namespace MaxCleanAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CustRegister()
+        {
+            if (RegisterResponse.register == null)
+            {
+                return NotFound(StatusCodes.Status404NotFound);
+            }
+            else
+            {
+                return Ok(RegisterResponse.register);
+            }
+        }
+        [HttpPost]
+        public IActionResult Addcustomer([FromBody]RegisterRequest model)
+        {
+            model.createddate = DateTime.Now;
+            model.updateddate = DateTime.Now;
+            RegisterResponse.Add(model);
+            return Created("Added successfully", model);
+        }
+        [Route("verification")]
+        [HttpGet]
+        
+        public IActionResult EmailMobileVerification([FromBody]RegisterRequest model)
+        {
+            RegisterResponse.EmaiAndMobileVerification(model.Email, model.Mobil);
+            return Ok(RegisterResponse.register);
+        }
+    }
+}
---
+++ MaxCleanAPI/DTO/RegisterDTO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaxCleanAPI.DTO
+{
+    public class RegisterRequest
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public Int64 Mobil { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool Mobilverfied { get; set; }
+        public bool Emailverified { get; set; }
+        public string Status { get; set; }
+        public DateTime createddate { get; set; }
+        public DateTime updateddate { get; set; }
+    }
+
+    public class RegisterResponse
+    {
+        public static List<RegisterRequest> register { get; set; }
+        public static List<RegisterRequest>  Add(RegisterRequest registerRequest)
+        {
+            register = new List<RegisterRequest>();
+            register.Add(registerRequest);
+            return register;
+        }
+        public static bool EmaiAndMobileVerification(string email,Int64 mobile)
+        {
+           RegisterRequest registerRequest= register.SingleOrDefault(x => x.Email == email && x.Mobil == mobile);
+            if(registerRequest!=null)
+            {
+                registerRequest.Emailverified = true;
+                registerRequest.Mobilverfied = true;
+                registerRequest.Status = "Mobil and Email erified";
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
